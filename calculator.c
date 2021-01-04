392e0c9fc6b8394e76361dc77fa2a50fa241a7ea
Mooooon-s <mmk990212@naver.com>
2021-01-04 13:16:39 +0900

    add squared

--- calculator.c
+++ calculator.c
@@ -1,30 +1,43 @@
-int main(void){
-    //calculator
-    int a,b;
-    int result;
-    char c;
+#include<stdio.h>
 
-    printf("enter the formula:");
-    scanf_s("%d" "%s" "%d", &a,&c,1,&b);
+int Squared(int a, int b) {
+	int result = a;
+	for (int i = 0; i < b-1; i++) {
+		result *= a;
+	}
+	return result;
+}
 
-    switch (c)
-    {
-    case '+':
-        result=a+b;
-        break;
-    case '-':
-        result = a-b;
-        break;
-    case '*':
-        result = a*b;
-        break;
-    case '/':
-        result=a/b;
-        break;
-    default:
-        break;
-    }
+int main(void) {
+	int first_num, second_num;
+	char Operator;
+	int result=0;
 
-    printf("result: %d",result);
+	while (1) {
+		printf("수식을 입력하시오:");
+		scanf_s("%d %c %d", &first_num, &Operator, 1, &second_num);
+
+		switch (Operator)
+		{
+		case '+':
+			result = first_num + second_num;
+			break;
+		case '-':
+			result = first_num - second_num;
+			break;
+		case '*':
+			result = first_num * second_num;
+			break;
+		case '/':
+			result = first_num / second_num;
+			break;
+		case '^':
+			result=Squared(first_num,second_num);
+			break;
+		}
+
+		printf("결과: %d\n", result);
+	}
+	return 0;
 
 }
