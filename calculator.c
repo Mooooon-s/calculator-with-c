9f44b8abc2e6fea5214d68ce0534af88f9864d86
Mooooon-s <mmk990212@naver.com>
2021-01-04 11:48:36 +0900

    commit test

--- calculator.c
+++ calculator.c
@@ -1,4 +1,30 @@
 int main(void){
     //calculator
-    
+    int a,b;
+    int result;
+    char c;
+
+    printf("enter the formula:");
+    scanf_s("%d" "%s" "%d", &a,&c,1,&b);
+
+    switch (c)
+    {
+    case '+':
+        result=a+b;
+        break;
+    case '-':
+        result = a-b;
+        break;
+    case '*':
+        result = a*b;
+        break;
+    case '/':
+        result=a/b;
+        break;
+    default:
+        break;
+    }
+
+    printf("result: %d",result);
+
 }
